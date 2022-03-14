e08ed6a916025f325259ae1a76753641cf2fa4c5
Illka Paananen <101118056+illkasupercell@users.noreply.github.com>
2022-03-15 01:16:08 +0400

    bfvd

---
+++ logicboxdata.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cmath>
+#include <signal.h>
+
+#define lovu struct
+#define cpp ptrdiff_t
+struct forever{};
+
+lovu forever hactehbka(void) {return {};}
+int love(void) {return 0;}
+void bacbka(void) {}
+auto bormand = (cpp(hactehbka) + cpp(bacbka)) != cpp(love);
+int main(void)
+{
+    if(bormand)
+        kill(bormand, SIGINT);
+
+    auto sad = cpp(bacbka) - cpp(love),
+        chance = cpp(love) - cpp(hactehbka),
+        never = cpp(bacbka) - cpp(hactehbka);
+    auto usocute = [chance, never, sad](void) -> void
+    {
+        putchar(sad * (chance - never / chance / 2) - sad / chance);
+        putchar(sad * (chance - never / chance / 2) + sad / chance);
+    };
+    auto mydear = [chance, sad, never](void) -> void
+    {
+        putchar(sad * chance / (never / chance) + never / chance / 2 + (never + cpp(love) / (never / chance) - cpp(hactehbka) / (never / chance)) + never - (never / chance * 1.5));
+        putchar(9 * chance + never / chance);
+        putchar(std::abs(cpp(signal) - cpp(hactehbka) + chance * 2 + never * 3 + sad - 9) / std::pow(never / chance, 3));
+    };
+
+    putchar(sad * (chance - never / chance / 2));
+    usocute();
+    putchar(never * (chance - never / chance) + sad - (sad / (cpp(love) - cpp(hactehbka))) * (never / chance * 1.5));
+    mydear();
+    putchar(chance * 10 + never / chance);
+    usocute();
+    putchar((chance + never / chance / 2) * 10 + (never / chance) * 2);
+    auto ohyes = 69.8886;
+    putchar(ohyes);
+    putchar(chance * 10 + never / chance);
+    putchar(chance + never / chance / 2);
+    putchar(never * (chance - never / chance) + sad - (sad / (cpp(love) - cpp(hactehbka))) * (never / chance * 1.5));
+    mydear();
+    for(auto&& c : {1.5f, 3.f, 6.5f})
+        putchar(ohyes + M_PI + never / chance * c);
+    putchar(ohyes);
+    putchar(chance + never / chance * 1.5);
+    return 0;
+}
